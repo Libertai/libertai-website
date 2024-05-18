46f7a5e976c061c2ccf347af929e43280b485d3f
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-19 00:06:16 +0200

    fix: Text size

--- src/pages/Home/DataSecuritySection.vue
+++ src/pages/Home/DataSecuritySection.vue
@@ -3,7 +3,7 @@
 		<div class="flex mx-20 gap-x-6">
 			<div class="m-6 w-1/2">
 				<h2 class="text-center mb-6">No Harvesting: You are in Control of Your Data</h2>
-				<p>
+				<p class="body-small">
 					Enjoy the peace of mind that comes with Libertai's robust security measures. Advanced encryption protocols,
 					distributed data storage, and rigorous access controls ensure that your data remains safeguarded against
 					unauthorized access, surveillance, or exploitation.
@@ -17,4 +17,3 @@
 		</div>
 	</div>
 </template>
-<script lang="ts" setup></script>
