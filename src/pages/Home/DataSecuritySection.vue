f47b4989e9d5d87b3f6a58e27e0c1b1d9fc9b97b
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-18 23:47:00 +0200

    feat: Data security section

---
+++ src/pages/Home/DataSecuritySection.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="flex py-48 bg-white">
+		<div class="flex mx-20 gap-x-6">
+			<div class="m-6 w-1/2">
+				<h2 class="text-center mb-6">No Harvesting: You are in Control of Your Data</h2>
+				<p>
+					Enjoy the peace of mind that comes with Libertai's robust security measures. Advanced encryption protocols,
+					distributed data storage, and rigorous access controls ensure that your data remains safeguarded against
+					unauthorized access, surveillance, or exploitation.
+				</p>
+			</div>
+			<div class="flex space w-1/2 gap-x-6 justify-evenly">
+				<img alt="No harvesting 1" height="408" src="../../assets/data1.svg" width="193" />
+				<img alt="No harvesting 2" height="408" src="../../assets/data2.svg" width="193" />
+				<img alt="No harvesting 3" height="408" src="../../assets/data3.svg" width="193" />
+			</div>
+		</div>
+	</div>
+</template>
+<script lang="ts" setup></script>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,11 +3,13 @@ import Hero from "./HeroSection.vue";
 import PartnersSection from "./PartnersSection.vue";
 import FooterSection from "./FooterSection.vue";
 import ChatPreviewSection from "./ChatPreviewSection.vue";
+import DataSecuritySection from "./DataSecuritySection.vue";
 </script>
 
 <template>
 	<Hero />
 	<PartnersSection />
 	<ChatPreviewSection />
+	<DataSecuritySection />
 	<FooterSection />
 </template>
