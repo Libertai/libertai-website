2f56f310367a8568815ab0ac2fc87049d08e2252
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-18 16:02:08 +0200

    feat: add all font format and custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,vue}"],
-  theme: {
-    colors: {
-      primary: "#3354FF",
-      secondary: "#D2DCFF",
-      white: "#F6F8FF",
-      neutral: {
-        100: "#FFFFFF",
-        200: "#F9F9F9",
-        300: "#ECF0FA",
-        400: "#D8DBE9",
-        500: "#AEB3C1",
-        600: "#474B64",
-        700: "#212438",
-        800: "#0E1435",
-      }
-    },
-    fontFamily: {
-      sans: ["Mulish", "sans-serif"
-
-      ],
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{ts,tsx,vue}"],
+	theme: {
+		colors: {
+			primary: "#3354FF",
+			secondary: "#D2DCFF",
+			white: "#F6F8FF",
+			neutral: {
+				100: "#FFFFFF",
+				200: "#F9F9F9",
+				300: "#ECF0FA",
+				400: "#D8DBE9",
+				500: "#AEB3C1",
+				600: "#474B64",
+				700: "#212438",
+				800: "#0E1435",
+			},
+		},
+		fontFamily: {
+			sans: ["Mulish", "sans-serif"],
+		},
+		fontSize: {
+			h1: ["48px", "56px"],
+			h2: ["36px", "44px"],
+		},
+		extend: {},
+	},
+	plugins: [],
 };
