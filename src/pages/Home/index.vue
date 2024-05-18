4a047568357b125d0361e5d0a3ee027c20195283
Croos3r <dorian.moy@epitech.eu>
2024-05-18 20:28:13 +0200

    feat(home): hero section
    Took 36 minutes
    
    Took 23 seconds

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 	import PartnersSection from "./PartnersSection.vue";
+	import Hero from "./HeroSection.vue";
 </script>;
 
 <template>
 	<PartnersSection />
+	<Hero />
 </template>;
