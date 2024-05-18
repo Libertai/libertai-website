6c14ccd8ae5b075118600e0b90e2409a09e4c006
Croos3r <dorian.moy@epitech.eu>
2024-05-18 19:36:42 +0200

    feat(components): added libertai branded button component
    Took 30 seconds
    
    
    Took 2 seconds

---
+++ src/components/LButton.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+
+import { twMerge } from "tailwind-merge";
+
+export type LButtonProps = {
+	text: string;
+	variant?: "primary" | "secondary";
+	light?: boolean;
+	small?: boolean;
+};
+const {
+	text,
+	variant,
+	light,
+	small
+} = withDefaults(defineProps<LButtonProps>(), {
+	variant: "primary",
+	light: false,
+	small: false
+});
+let style = twMerge("rounded-full font-bold", small ? "body-tiny px-6 py-3" : "body-default px-8 py-4");
+
+if (variant === "primary")
+	style = twMerge(style, light ? "bg-neutral-100 text-primary" : "bg-primary text-neutral-100");
+else
+	style = twMerge(style, "border bg-transparent", light ? "text-neutral-100 border-neutral-100" : "text-neutral-800 border-neutral-400");
+</script>
+
+<template>
+	<button :class="style">
+		{{ text }}
+	</button>
+</template>
