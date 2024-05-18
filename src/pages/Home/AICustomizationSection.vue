4d73ba5ce82d36cf59584c07fff89d7ff384d68b
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-18 22:32:01 +0200

    feat: base for the section

---
+++ src/pages/Home/AICustomizationSection.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="py-40" style="background-color: #dee1ff">
+		<div class="bg-neutral-100" style="height: 555px"></div>
+	</div>
+</template>
+
+<style scoped></style>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,10 +2,12 @@
 import Hero from "./HeroSection.vue";
 import PartnersSection from "./PartnersSection.vue";
 import FooterSection from "./FooterSection.vue";
+import AICustomizationSection from "./AICustomizationSection.vue";
 </script>
 
 <template>
 	<Hero />
 	<PartnersSection />
+	<AICustomizationSection />
 	<FooterSection />
 </template>
