52e1e0f38e7d1ae56e2fda4430c6ffa48d6e7481
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-18 19:41:34 +0200

    feat: create footer

---
+++ src/components/Home/FooterSection.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer
+          class="relative bg-footer pt-72"
+          style="width: 100%; height: 910px"
+  >
+    <div class="flex flex-col items-center justify-center space-y-10">
+      <img src="../../assets/logo_white.svg" alt="logo" style="width: 105px; height: 105px" />
+      <p class="text-center text-primary text-tiny mt-4 body-large">Stay Connected</p>
+      <div class="flex flex-row space-x-7">
+        <a href="https://www.facebook.com/" target="_blank">
+          <img src="../../assets/X.svg" alt="facebook" style="width: 36px; height: 36px" />
+        </a>
+        <a href="https://www.instagram.com/" target="_blank">
+          <img src="../../assets/Telegram.svg" alt="instagram" style="width: 36px; height: 36px" />
+        </a>
+        <a href="https://www.linkedin.com/" target="_blank">
+          <img src="../../assets/ALEPH.IM.svg" alt="linkedin" style="width: 36px; height: 36px" />
+        </a>
+      </div>
+      <p class="text-center text-neutral-300 text-tiny mt-2 body-large">
+        Be part of a pioneering community valuing individual privacy and autonomy<br/>
+        Join a vibrant community of AI enthusiasts,developers, and innovators who<br/>
+        share a common commitment to privacy and decentralization. Collaborate,<br/>
+        share insights, and contribute to the continuous evolution of Libertai's AI<br/>
+        ecosystem, shaping the future of decentralized intelligence.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
