8595e450df711825f35dd78f0b45fe76ad02565e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-18 21:07:35 +0200

    fix: Footer link opens in new page

--- src/pages/Home/FooterSection.vue
+++ src/pages/Home/FooterSection.vue
@@ -28,11 +28,9 @@
 			<p class="text-center text-neutral-300 text-tiny mt-2 body-default font-bold">
 				Copyright © LibertAI.io | Powered by
 			</p>
-			<a href="https://aleph.im/">
+			<a href="https://aleph.im/" target="_blank">
 				<p class="text-center text-primary text-tiny mt-2 body-default font-bold">aleph.im</p>
 			</a>
 		</div>
 	</footer>
 </template>
-<script setup lang="ts"></script>
-<script setup lang="ts"></script>
