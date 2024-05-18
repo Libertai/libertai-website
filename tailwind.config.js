cc561871474dc196a67feb11a4cd93fb57a574e9
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-18 14:47:08 +0200

    feat: setup colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,vue}"],
   theme: {
+    colors: {
+      primary: "#3354FF",
+      secondary: "#D2DCFF",
+      white: "#F6F8FF",
+      neutral: {
+        100: "#FFFFFF",
+        200: "#F9F9F9",
+        300: "#ECF0FA",
+        400: "#D8DBE9",
+        500: "#AEB3C1",
+        600: "#474B64",
+        700: "#212438",
+        800: "#0E1435",
+      }
+    },
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"
+
+      ],
+    },
     extend: {},
   },
   plugins: [],
