e4014d635f1a1737fb2e6771433e58a7ce30f9de
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-18 16:03:33 +0200

    feat: add design page to check design

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import CompanyPage from "./pages/CompanyPage.vue";
+import DesignPage from "./pages/DesignPage.vue";
 
 const routes = [
 	{ path: "/", component: HomePage },
 	{ path: "/company", component: CompanyPage },
+	{ path: "/design", component: DesignPage },
 ];
 
 export const router = createRouter({
