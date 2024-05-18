5c483c25b9e06456e78b72d8f0f9417c42e4ae0d
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-18 20:33:44 +0200

    fix: Feedbacks

--- src/pages/Home/FooterSection.vue
+++ src/pages/Home/FooterSection.vue
@@ -1,17 +1,17 @@
 <template>
 	<footer class="relative bg-footer pt-72" style="width: 100%; height: 910px">
 		<div class="flex flex-col items-center justify-center space-y-10">
-			<img alt="logo" src="../../assets/logo_white.svg" style="width: 105px; height: 105px" />
+			<img alt="logo" src="../../assets/icons/logo_white.svg" style="width: 105px; height: 105px" />
 			<p class="text-center text-primary text-tiny mt-4 body-large">Stay Connected</p>
 			<div class="flex flex-row space-x-7">
-				<a href="https://www.facebook.com/" target="_blank">
-					<img alt="facebook" src="../../assets/X.svg" style="width: 36px; height: 36px" />
+				<a href="https://x.com/Libertai_DAI" target="_blank">
+					<img alt="Twitter / X" src="../../assets/icons/x.svg" style="width: 36px; height: 36px" />
 				</a>
-				<a href="https://www.instagram.com/" target="_blank">
-					<img alt="instagram" src="../../assets/Telegram.svg" style="width: 36px; height: 36px" />
+				<a href="https://t.me/liberchat_bot" target="_blank">
+					<img alt="Telegram" src="../../assets/icons/telegram.svg" style="width: 36px; height: 36px" />
 				</a>
-				<a href="https://www.linkedin.com/" target="_blank">
-					<img alt="linkedin" src="../../assets/ALEPH.IM.svg" style="width: 36px; height: 36px" />
+				<a href="https://aleph.im/" target="_blank">
+					<img alt="Aleph.im" src="../../assets/icons/aleph.svg" style="width: 36px; height: 36px" />
 				</a>
 			</div>
 			<p class="text-center text-neutral-300 text-tiny mt-2 body-large max-sm:body-tiny max-lg:body-default">
