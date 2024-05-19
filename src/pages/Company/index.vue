b567a336b36263b885330ac2f974c4df3b56073f
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-19 23:42:49 +0200

    feat: Company hero

--- src/pages/Company/index.vue
+++ src/pages/Company/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import ProductTextsSection from "./ProductTextsSection.vue";
 import TeamTextsSection from "./TeamTextsSection.vue";
+import PioneeringForceSection from "./PioneeringForceSection.vue";
 </script>
 
 <template>
+	<PioneeringForceSection />
 	<ProductTextsSection />
 	<TeamTextsSection />
 </template>
