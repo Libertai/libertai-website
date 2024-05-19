b641e6a17f1e73be0125203405495f8a37485dc3
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-20 00:38:42 +0200

    feat: Developer hero section

---
+++ src/pages/Developer/DeveloperHeroSection.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="bg-white grid md:grid-cols-[40%_60%]">
+		<div class="flex flex-col justify-center items-center text-center max-w-xl m-auto gap-y-6">
+			<h2>Where AI Meets Freedom</h2>
+			<p class="body-small">
+				Libertai fosters a community-driven approach to AI development, encouraging collaboration, knowledge sharing,
+				and collective innovation. Be part of a vibrant ecosystem that shapes the future of AI.
+			</p>
+		</div>
+		<img alt="Company" class="max-md:max-h-80 m-auto" src="../../assets/developer.svg" />
+	</div>
+</template>
