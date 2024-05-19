eaa237e56d026ca5c361d832a2e34d31e21ea30c
Reza Rahemtola <reza.rahemtola@poc-innovation.fr>
2024-05-19 19:21:14 +0200

    feat: Developer unstoppable network section (#13)

---
+++ src/pages/Developer/NetworkSection.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="architecture text-neutral-100 py-96">
+		<div class="mb-24 text-center">
+			<h2 class="mb-6">An unstoppable inter planetary network.</h2>
+			<p class="body-small">
+				Libertai large language models are running on a set of technologies such as IPFS in combination with aleph.im.
+				It effectively runs on a fully decentralized, uncensored, secure and resilient computing network that is
+				practically unstoppable.
+			</p>
+		</div>
+		<img alt="Architecture" class="m-auto" src="../../assets/architecture.svg" />
+	</div>
+</template>
+
+<style scoped>
+.architecture {
+	background-color: #030014;
+}
+</style>
---
+++ src/pages/Developer/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import NetworkSection from "./NetworkSection.vue";
+</script>
+
+<template>
+	<NetworkSection />
+</template>
