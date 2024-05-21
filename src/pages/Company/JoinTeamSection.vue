6fb29d7cc789a24f3f15b7cef0d5faa97ba27035
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-21 22:20:24 +0200

    feat: Company join team section

---
+++ src/pages/Company/JoinTeamSection.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="grid bg-white py-44 md:grid-cols-[40%_60%]">
+		<div class="m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center">
+			<h2>Be a Part of the AI Revolution</h2>
+			<p class="body-small">
+				Are you passionate about artificial intelligence, decentralization, and community-driven innovation? Do you want
+				to be a part of a pioneering team that is transforming the AI landscape? Look no further than Libertai!
+			</p>
+			<a class="max-sm:mb-6" href="mailto:hello@libertai.io" target="_blank">
+				<l-button small text="Join Now" />
+			</a>
+		</div>
+		<img alt="Company" class="m-auto max-md:max-h-80" src="../../assets/team.png" />
+	</div>
+</template>
+<script lang="ts" setup>
+import LButton from "../../components/LButton.vue";
+</script>
--- src/pages/Company/index.vue
+++ src/pages/Company/index.vue
@@ -2,10 +2,12 @@
 import ProductTextsSection from "./ProductTextsSection.vue";
 import TeamTextsSection from "./TeamTextsSection.vue";
 import PioneeringForceSection from "./CompanyHeroSection.vue";
+import JoinTeamSection from "./JoinTeamSection.vue";
 </script>
 
 <template>
 	<PioneeringForceSection />
 	<ProductTextsSection />
+	<JoinTeamSection />
 	<TeamTextsSection />
 </template>
