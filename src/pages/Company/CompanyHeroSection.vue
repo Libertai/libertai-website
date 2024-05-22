a773983af7c607ed53c7226d9eee4d9a12593160
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-22 14:50:34 +0200

    feat: company page responsive

--- src/pages/Company/CompanyHeroSection.vue
+++ src/pages/Company/CompanyHeroSection.vue
@@ -1,14 +1,22 @@
 <template>
-	<div class="grid bg-white md:grid-cols-[60%_40%]">
-		<div class="m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center">
-			<h2>A Pioneering Force in Decentralized AI</h2>
-			<p class="body-small">
+	<div class="relative grid overflow-hidden bg-white lg:grid-cols-[60%_40%]">
+		<img
+			alt="Company"
+			class="m-auto max-lg:absolute max-lg:h-screen max-lg:opacity-65 max-lg:blur-sm lg:hidden"
+			src="../../assets/company.png"
+		/>
+		<div
+			class="z-10 m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center max-lg:gap-y-12 max-lg:px-4 max-lg:py-40"
+		>
+			<h2 class="block text-left max-lg:text-center max-sm:hidden">A Pioneering Force in Decentralized AI</h2>
+			<h3 class="hidden max-sm:block">A Pioneering Force in Decentralized AI</h3>
+			<p class="body-small text-left max-lg:text-center">
 				Libertai is a revolutionary company that is transforming the AI landscape by developing decentralized AI
 				solutions. Founded on the principles of transparency, security, and community-driven innovation, Libertai is
 				committed to creating a new paradigm for artificial intelligence.
 			</p>
 		</div>
-		<img alt="Company" class="m-auto max-md:max-h-80" src="../../assets/company.png" />
+		<img alt="Company" class="m-auto max-lg:hidden" src="../../assets/company.png" />
 	</div>
 </template>
 <script lang="ts" setup></script>
--- src/pages/Company/JoinTeamSection.vue
+++ src/pages/Company/JoinTeamSection.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="grid bg-white py-44 md:grid-cols-[40%_60%]">
+	<div class="grid bg-white py-44 max-lg:pb-0 max-lg:pt-16 lg:grid-cols-[40%_60%]">
 		<div class="m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center">
 			<h2>Be a Part of the AI Revolution</h2>
 			<p class="body-small">
 				Are you passionate about artificial intelligence, decentralization, and community-driven innovation? Do you want
 				to be a part of a pioneering team that is transforming the AI landscape? Look no further than Libertai!
 			</p>
-			<a class="max-sm:mb-6" href="mailto:hello@libertai.io" target="_blank">
+			<a class="max-lg:mb-6" href="mailto:hello@libertai.io" target="_blank">
 				<l-button small text="Join Now" />
 			</a>
 		</div>
-		<img alt="Company" class="m-auto max-md:max-h-80" src="../../assets/team.png" />
+		<img alt="Company" class="m-auto max-lg:w-full lg:rounded-2xl" src="../../assets/team.jpg" width="730px" />
 	</div>
 </template>
 <script lang="ts" setup>
