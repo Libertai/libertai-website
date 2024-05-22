6a33940fcfd0fabfb0737ad1b60027f62d442999
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-22 21:05:26 +0200

    fix: Data Security section

--- src/pages/Home/DataSecuritySection.vue
+++ src/pages/Home/DataSecuritySection.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="flex bg-white py-48">
-		<div class="mx-20 flex justify-center gap-x-6 max-lg:mx-8">
-			<div class="z-10 m-6 w-1/2 max-lg:w-full max-lg:space-y-8">
-				<h2 class="mb-6 block text-center max-xl:hidden">No Harvesting: You are in Control of Your Data</h2>
+	<section class="flex justify-center bg-white">
+		<div class="flex justify-center gap-x-6 bg-white px-24 py-48 max-lg:px-6" style="width: 1440px">
+			<div class="z-10 w-1/2 max-lg:w-full max-lg:space-y-8">
+				<h2 class="mb-6 block text-left max-xl:hidden">No Harvesting: You are in Control of Your Data</h2>
 				<h3 class="mb-6 hidden text-center max-xl:block">No Harvesting: You are in Control of Your Data</h3>
-				<p class="body-small">
+				<p class="body-small text-left max-lg:text-center">
 					Enjoy the peace of mind that comes with Libertai's robust security measures. Advanced encryption protocols,
 					distributed data storage, and rigorous access controls ensure that your data remains safeguarded against
 					unauthorized access, surveillance, or exploitation.
@@ -13,11 +13,11 @@
 			<div
 				class="space z-0 flex w-1/2 justify-evenly gap-x-6 overflow-hidden max-lg:absolute max-lg:w-auto max-lg:opacity-40 max-lg:blur-sm"
 			>
-				<img class="max-xl:hidden" alt="No harvesting 1" height="408" src="../../assets/data1.png" width="193" />
-				<img alt="No harvesting 2" height="408" src="../../assets/data2.png" width="193" />
-				<img alt="No harvesting 3" height="408" src="../../assets/data3.png" width="193" />
+				<img class="max-xl:hidden" alt="No harvesting 1" height="408" src="../../assets/data1.png" width="180" />
+				<img alt="No harvesting 2" height="408" src="../../assets/data2.png" width="180" />
+				<img alt="No harvesting 3" height="408" src="../../assets/data3.png" width="180" />
 			</div>
 		</div>
-	</div>
+	</section>
 </template>
 <script setup lang="ts"></script>
