a629f6a5b53356c7c7480d0f56d47104e696d5ce
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-22 14:57:45 +0200

    feat: Earn page responsive

--- src/pages/Earn/EarnHeroSection.vue
+++ src/pages/Earn/EarnHeroSection.vue
@@ -1,13 +1,22 @@
 <template>
-	<div class="grid bg-white md:grid-cols-[50%_50%]">
-		<div class="m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center">
-			<h2>Decentralize everything and earn LibertAI Tokens</h2>
-			<p class="body-small">
+	<div class="relative grid overflow-hidden bg-white lg:grid-cols-[50%_50%]">
+		<img
+			alt="Earn"
+			class="m-auto max-lg:absolute max-lg:h-screen max-lg:opacity-65 max-lg:blur-sm lg:hidden"
+			src="../../assets/earn.png"
+		/>
+		<div
+			class="z-10 m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center max-lg:gap-y-12 max-lg:px-4 max-lg:py-40"
+		>
+			<h2 class="block text-left max-lg:text-center max-sm:hidden">Decentralize everything and earn LibertAI Tokens</h2>
+			<h3 class="hidden max-sm:block">Decentralize everything and earn LibertAI Tokens</h3>
+			<p class="body-small text-left max-lg:text-center">
 				Are you ready to dive into a new world of decentralized computing, where your contributions are rewarded with
 				tangible benefits? Look no further than Libertai Tokens! These Tokens play an essential role within the exciting
 				and innovative LibertAI ecosystem.
 			</p>
 		</div>
-		<img alt="Earn" class="m-auto max-md:max-h-80" src="../../assets/earn.png" />
+		<img alt="Earn" class="m-auto max-lg:hidden" src="../../assets/earn.png" />
 	</div>
 </template>
+<script setup lang="ts"></script>
