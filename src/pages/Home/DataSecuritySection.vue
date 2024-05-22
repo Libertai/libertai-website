7c771623e74967d70902833b14779bf1f9266bc4
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-22 11:54:06 +0200

    fix: responsive on really small device

--- src/pages/Home/DataSecuritySection.vue
+++ src/pages/Home/DataSecuritySection.vue
@@ -11,7 +11,7 @@
 				</p>
 			</div>
 			<div
-				class="space z-0 flex w-1/2 justify-evenly gap-x-6 max-lg:absolute max-lg:w-auto max-lg:opacity-40 max-lg:blur-sm"
+				class="space z-0 flex w-1/2 justify-evenly gap-x-6 overflow-hidden max-lg:absolute max-lg:w-auto max-lg:opacity-40 max-lg:blur-sm"
 			>
 				<img class="max-xl:hidden" alt="No harvesting 1" height="408" src="../../assets/data1.png" width="193" />
 				<img alt="No harvesting 2" height="408" src="../../assets/data2.png" width="193" />
