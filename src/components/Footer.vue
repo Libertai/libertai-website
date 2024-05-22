56f7b868d49037d319d6643aa79b6350ed7ee3e4
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-22 15:28:28 +0200

    fix: background image on responsive design + texts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex h-24 items-center justify-center space-x-2 bg-neutral-800 text-center">
+	<div class="z-30 flex h-24 items-center justify-center space-x-2 bg-neutral-800 text-center">
 		<p class="text-tiny body-default max-lg:body-small mt-2 text-center font-bold text-neutral-300">
 			Copyright © LibertAI.io | Powered by
 		</p>
--- src/pages/Company/CompanyHeroSection.vue
+++ src/pages/Company/CompanyHeroSection.vue
@@ -1,12 +1,8 @@
 <template>
-	<div class="relative grid overflow-hidden bg-white lg:grid-cols-[60%_40%]">
-		<img
-			alt="Company"
-			class="m-auto max-lg:absolute max-lg:h-screen max-lg:opacity-75 max-lg:blur-sm lg:hidden"
-			src="../../assets/company.png"
-		/>
+	<div class="image-background relative grid overflow-hidden bg-white lg:grid-cols-[60%_40%]">
+		<div class="absolute left-0 top-0 z-10 h-full w-full bg-white max-lg:hidden"></div>
 		<div
-			class="z-10 m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center max-lg:gap-y-12 max-lg:px-4 max-lg:py-40"
+			class="z-10 m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center max-lg:gap-y-12 max-lg:px-4 max-lg:py-40 max-lg:text-neutral-100"
 		>
 			<h2 class="block text-left max-lg:text-center max-sm:hidden">A Pioneering Force in Decentralized AI</h2>
 			<h3 class="hidden max-sm:block">A Pioneering Force in Decentralized AI</h3>
@@ -16,7 +12,17 @@
 				committed to creating a new paradigm for artificial intelligence.
 			</p>
 		</div>
-		<img alt="Company" class="m-auto max-lg:hidden" src="../../assets/company.png" />
+		<img alt="Company" class="z-10 m-auto max-lg:hidden" src="../../assets/company.png" />
 	</div>
 </template>
+
+<style scoped>
+div.image-background {
+	background-image: url("../../assets/company.png");
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+}
+</style>
+
 <script lang="ts" setup></script>
