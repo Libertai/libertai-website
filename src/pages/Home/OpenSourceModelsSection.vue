494fa1c50d982b308d05aa89bd1b3bdeabb5067f
Croos3r <dorian.moy@epitech.eu>
2024-05-23 02:04:00 +0200

    refactor(texts): now using centralized text configuration file for text with several use cases
    Took 33 minutes
    
    
    Took 2 seconds

--- src/pages/Home/OpenSourceModelsSection.vue
+++ src/pages/Home/OpenSourceModelsSection.vue
@@ -1,39 +1,7 @@
 <script lang="ts" setup>
 import LFlashCard from "../../components/LFlashCard.vue";
 import LButton from "../../components/LButton.vue";
-
-const models: Array<{ title: string; description: string }> = [
-	{
-		title: "Nous Hermes 2 Pro (Llama 3 8B)",
-		description:
-			"Hermes 2 Pro is an upgraded, retrained version of Nous Hermes 2, consisting of an updated and cleaned version of the OpenHermes 2.5 Dataset, as well as a newly introduced Function Calling and JSON Mode dataset developed in-house.\n",
-	},
-	{
-		title: "NeuralBeagle (7B)",
-		description:
-			"Hermes 2 Pro is an upgraded, retrained version of Nous Hermes 2, consisting of an updated and cleaned version of the OpenHermes 2.5 Dataset, as well as a newly introduced Function Calling and JSON Mode dataset developed in-house.\n",
-	},
-	{
-		title: "Mixtral (8x7B MOE)",
-		description:
-			"Hermes 2 Pro is an upgraded, retrained version of Nous Hermes 2, consisting of an updated and cleaned version of the OpenHermes 2.5 Dataset, as well as a newly introduced Function Calling and JSON Mode dataset developed in-house.\n",
-	},
-	{
-		title: "Nous Hermes 2 (34B)",
-		description:
-			"Hermes 2 Pro is an upgraded, retrained version of Nous Hermes 2, consisting of an updated and cleaned version of the OpenHermes 2.5 Dataset, as well as a newly introduced Function Calling and JSON Mode dataset developed in-house.\n",
-	},
-	{
-		title: "Llama 3 Instruct (70B)",
-		description:
-			"Hermes 2 Pro is an upgraded, retrained version of Nous Hermes 2, consisting of an updated and cleaned version of the OpenHermes 2.5 Dataset, as well as a newly introduced Function Calling and JSON Mode dataset developed in-house.\n",
-	},
-	{
-		title: "DeepSeek Coder (6.7B)",
-		description:
-			"Hermes 2 Pro is an upgraded, retrained version of Nous Hermes 2, consisting of an updated and cleaned version of the OpenHermes 2.5 Dataset, as well as a newly introduced Function Calling and JSON Mode dataset developed in-house.\n",
-	},
-];
+import { models } from "../../texts.ts";
 </script>
 
 <template>
