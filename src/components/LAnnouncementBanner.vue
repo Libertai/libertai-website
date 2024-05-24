794b1bc350fe6fd5a1baa700a630455a2d3368a0
Dorian Moy <dorian.moy@epitech.eu>
2024-05-25 00:35:51 +0200

    feat(announcement-banner): made internal component LAnnouncementBanner to integrate later (#35)

---
+++ src/components/LAnnouncementBanner.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+export type LAnnouncementBannerProps = {
+	text: string;
+	link?: {
+		href: string;
+		text: string;
+	};
+};
+withDefaults(defineProps<LAnnouncementBannerProps>(), {
+	link: undefined,
+});
+</script>
+
+<template>
+	<p class="bg-majorelle-500 pb-5 pt-8 text-center text-neutral-white">
+		{{ text }}
+		<RouterLink v-if="link && link.href.startsWith('/')" :to="link.href" class="font-bold">{{ link.text }}</RouterLink>
+		<a v-else-if="link" :href="link.href" class="font-bold">{{ link.text }}</a>
+	</p>
+</template>
