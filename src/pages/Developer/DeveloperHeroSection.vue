b17b1b796890cd8f8d28726b6fce3ee917142b9f
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-25 12:30:56 +0200

    feat: Add link to documentation

--- src/pages/Developer/DeveloperHeroSection.vue
+++ src/pages/Developer/DeveloperHeroSection.vue
@@ -5,17 +5,25 @@
 			<div
 				class="z-10 m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 pl-8 text-center text-majorelle-800 max-lg:gap-y-12 max-lg:px-4 max-lg:py-40 max-lg:text-neutral-white"
 			>
-				<h2 class="block text-left max-lg:text-center max-sm:hidden">Where AI Meets Freedom</h2>
+				<h2 class="block max-sm:hidden lg:text-left">Where AI Meets Freedom</h2>
 				<h3 class="hidden max-sm:block">Where AI Meets Freedom</h3>
-				<p class="body-small text-left max-lg:text-center">
+				<p class="body-small lg:text-left">
 					Libertai fosters a community-driven approach to AI development, encouraging collaboration, knowledge sharing,
 					and collective innovation. Be part of a vibrant ecosystem that shapes the future of AI.
 				</p>
+				<a href="https://docs.libertai.io">
+					<LButton class="w-fit max-sm:hidden" text="Documentation" />
+					<LButton class="hidden w-fit max-sm:block" light text="Documentation" />
+				</a>
 			</div>
 			<img alt="Developer" class="z-10 m-auto max-lg:hidden" src="../../assets/developer.png" />
 		</div>
 	</section>
 </template>
+
+<script lang="ts" setup>
+import LButton from "../../components/LButton.vue";
+</script>
 
 <style scoped>
 div.image-background {
@@ -25,5 +33,3 @@ div.image-background {
 	background-repeat: no-repeat;
 }
 </style>
-
-<script setup lang="ts"></script>
