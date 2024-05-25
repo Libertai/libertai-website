652b16d0f82797dc1eee4cfb7ab4153b3797730e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-25 12:33:23 +0200

    fix: Landing footer design

--- src/pages/Home/FooterSection.vue
+++ src/pages/Home/FooterSection.vue
@@ -3,7 +3,6 @@
 		<div class="relative w-full pt-72" style="height: 910px; width: 1440px">
 			<div class="flex flex-col items-center justify-center space-y-10">
 				<img alt="logo" src="../../assets/icons/logo_white.svg" style="width: 105px; height: 105px" />
-				<p class="text-tiny body-large mt-4 text-center text-majorelle-500">Stay Connected</p>
 				<div class="flex flex-row space-x-7">
 					<a href="https://x.com/Libertai_DAI" target="_blank">
 						<img alt="Twitter / X" src="../../assets/icons/x.svg" style="width: 36px; height: 36px" />
@@ -15,6 +14,7 @@
 						<img alt="Aleph.im" src="../../assets/icons/aleph.svg" style="width: 36px; height: 36px" />
 					</a>
 				</div>
+				<p class="text-tiny body-large mt-4 text-center font-bold text-majorelle-500">Stay connected</p>
 				<p class="text-tiny body-small max-sm:body-tiny mt-2 max-w-xl px-8 text-center text-neutral-300">
 					Be part of a pioneering community valuing individual privacy and autonomy Join a vibrant community of AI
 					enthusiasts,developers, and innovators who share a common commitment to privacy and decentralization.
