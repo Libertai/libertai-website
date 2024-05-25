9cfce00c96a52d4a04784d8364f0d6c6ce9da6d6
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-05-25 12:08:49 +0200

    fix: New landing hero text

--- src/pages/Home/HeroSection.vue
+++ src/pages/Home/HeroSection.vue
@@ -9,13 +9,7 @@ import LButton from "../../components/LButton.vue";
 				<h1 class="max-lg:hidden">Discover the Freedom of Decentralized AI</h1>
 				<h2 class="hidden sm:max-lg:block">Discover the Freedom of Decentralized AI</h2>
 				<h3 class="hidden font-bold max-sm:block">Discover the Freedom of Decentralized AI</h3>
-				<p class="body-small">
-					Experience the privacy of LibertAI's decentralized AI platform - chat freely, without data logging. Explore
-					various Chat-GPT like open-source models, like Llama 3 70b, for free. Create personalized AI assistants for
-					enhanced productivity.<br />
-					<br />
-					Your AI journey, your privacy!
-				</p>
+				<p class="body-small">Use decentralized AI and keep your data private and secure with blockchain.</p>
 				<a class="max-lg:mt-8" href="https://chat.libertai.io">
 					<LButton class="w-fit" light text="TRY FOR FREE" />
 				</a>
