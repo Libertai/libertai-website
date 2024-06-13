a7d9aed42947342aaf9ae63ce5c0c2bc097083cc
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-06-13 23:21:21 +0200

    feat: add Hero section

---
+++ src/pages/Tokenomix/TokenomicsHeroSection.vue
@@ -0,0 +1,31 @@
+<template>
+	<section class="relative flex justify-center">
+		<div class="image-background bg-white relative grid overflow-hidden lg:grid-cols-[50%_50%]" style="width: 1440px">
+			<div class="absolute left-0 top-0 z-10 h-full w-full bg-majorelle-100 max-lg:hidden" />
+			<div
+				class="z-20 m-auto flex max-w-xl flex-col items-center justify-center gap-y-6 text-center text-majorelle-800 max-lg:gap-y-12 max-lg:px-4 max-lg:py-40 max-lg:text-neutral-white"
+			>
+				<h2 class="block w-full text-left max-lg:text-center max-sm:hidden lg:text-majorelle-500">
+					LibertAI Tokenomics
+				</h2>
+				<h3 class="hidden text-neutral-white max-sm:block">LibertAI Tokenomics</h3>
+				<p class="body-small text-left max-lg:text-center">
+					These tokenomics components work together to create a balanced and incentive-driven ecosystem that encourages
+					participation, innovation, and growth within the LibertAI platform.
+				</p>
+			</div>
+			<img alt="Earn" class="z-10 m-auto max-lg:hidden" src="../../assets/image 32.png" />
+		</div>
+	</section>
+</template>
+
+<style scoped>
+div.image-background {
+	background-image: url("../../assets/image 32.png");
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+}
+</style>
+
+<script setup lang="ts"></script>
