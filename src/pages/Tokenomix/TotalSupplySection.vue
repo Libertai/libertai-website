edeb7cb1988c07945f1030bf20b162a07690397b
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-06-14 11:01:17 +0200

    feat: base for sections

---
+++ src/pages/Tokenomix/TotalSupplySection.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section class="relative flex justify-center py-8">
+		<div class="relative grid overflow-hidden" style="width: 1440px">
+			<div>TotalSupplySection</div>
+		</div>
+	</section>
+</template>
+
+<style scoped></style>
--- src/pages/Tokenomix/index.vue
+++ src/pages/Tokenomix/index.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import TokenomicsHeroSection from "./TokenomicsHeroSection.vue";
+import TotalSupplySection from "./TotalSupplySection.vue";
+import OverallTotalSupplySection from "./OverallTotalSupplySection.vue";
+import VestingScheduleSection from "./VestingScheduleSection.vue";
+import RaiseSection from "./RaiseSection.vue";
+import AirdropNetworkSection from "./AirdropNetworkSection.vue";
+import TokenUseSection from "./TokenUseSection.vue";
 </script>
 
 <template>
 	<TokenomicsHeroSection />
+	<TotalSupplySection />
+	<OverallTotalSupplySection />
+	<VestingScheduleSection />
+	<RaiseSection />
+	<AirdropNetworkSection />
+	<TokenUseSection />
 </template>
