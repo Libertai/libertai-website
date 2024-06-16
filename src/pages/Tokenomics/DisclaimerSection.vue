642abd272549be8658588443fc622ce7d9a25935
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-06-16 22:43:48 +0200

    feat: Tokenomics disclaimer

---
+++ src/pages/Tokenomics/DisclaimerSection.vue
@@ -0,0 +1,25 @@
+<template>
+	<section class="relative flex justify-center">
+		<div class="relative overflow-hidden px-20 py-12 max-lg:px-12" style="width: 1440px">
+			<div class="justify-center rounded-xl px-12 py-6 max-lg:px-4">
+				<p class="body-bold-large pb-4 text-center text-majorelle-700">Disclaimer</p>
+				<p class="body-bold-small pb-4 text-center text-majorelle-700">
+					The LibertAI Platform is still under development and may undergo significant changes over time. Although
+					LibertAI Platform has certain features and specifications that are set forth in the litepaper and website, the
+					team may make changes to such features and specifications for any number of reasons, any of which may mean
+					that the LibertAI Token Platform does not meet the holders expectations. Furthermore, it is specified that
+					future situations or a change in circumstances may lead the team to bring, at its sole discretion, amendments,
+					changes or updates to the Tokens’ tokenomics, including without limitation the maximum supply, vesting
+					schedules or token distribution ratios, allocation or timings.
+					<br /><br />
+					The tokens do not provide the holders with rights of any form with respect to the company/team or its revenues
+					or assets, including, but not limited to, any voting, distribution, redemption, liquidation, proprietary
+					(including all forms of intellectual property), or other financial or legal rights; (ii) are not a loan to the
+					company and do not entitle the holder to repayment of principal amount or interest; and (iii) do not provide
+					the holder with any ownership or other interest in company such as rights to any return, or any
+					profit-participation right or rights to payments of any kind whatsoever.
+				</p>
+			</div>
+		</div>
+	</section>
+</template>
