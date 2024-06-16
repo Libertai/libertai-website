5a7985542d74ee3665d026a8da22130565718bdd
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-06-16 17:09:18 +0200

    fix: responsive Vesting Schedule Cards

--- src/pages/Tokenomics/VestingScheduleSection.vue
+++ src/pages/Tokenomics/VestingScheduleSection.vue
@@ -51,16 +51,18 @@ const stats = [
 						<img alt="Vesting" class="max-sm:hidden" src="../../assets/vesting.png" />
 						<img alt="Vesting" class="mx-auto sm:hidden" src="../../assets/vesting_mobile.png" />
 					</div>
-					<div class="flex flex-col justify-center space-y-4 max-lg:px-4 max-md:pt-5">
+					<div class="flex flex-col justify-center space-y-4 max-md:pt-5">
 						<div
 							v-for="stat in stats"
 							:key="stat.name"
 							class="relative ml-5 mr-3 flex h-24 items-center justify-center rounded-3xl shadow-lg max-md:mx-0"
 						>
-							<div class="flex justify-around pr-8">
+							<div class="flex justify-center max-lg:flex-col max-lg:px-4">
 								<div :class="stat.color + ' absolute left-0 top-0 flex h-full w-5 items-center justify-center'" />
-								<p class="body-bold-default w-48 text-left text-majorelle-700">{{ stat.name }}</p>
-								<div class="flex items-center">
+								<p class="body-bold-default w-48 text-left text-majorelle-700 max-lg:w-auto max-lg:text-center">
+									{{ stat.name }}
+								</p>
+								<div class="flex items-center max-lg:justify-center">
 									<p class="body-bold-default pr-1 text-center text-majorelle-700">{{ stat.value }}</p>
 									<p class="body-default text-center text-majorelle-400">({{ stat.percent }})</p>
 								</div>
