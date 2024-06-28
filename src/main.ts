28baefd4169b1c83efd39995a1e1d195070f85c6
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-06-29 00:30:56 +0200

    feat: Remove token unlock countdown

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { router } from "./router.ts";
-import { createPinia } from "pinia";
 
-const pinia = createPinia();
-
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App).use(router).mount("#app");
