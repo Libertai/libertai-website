a85cc7337979445ba86bf3a8fdc19884749e2140
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-07-02 01:18:32 +0200

    feat(home): Link to partners

--- src/pages/Home/PartnersSection.vue
+++ src/pages/Home/PartnersSection.vue
@@ -4,11 +4,15 @@
 			<div class="flex h-full flex-col items-center justify-center">
 				<p class="body-bold-large text-center text-majorelle-800">Proudly supported by</p>
 				<div class="mt-6 flex max-sm:flex-col max-sm:space-y-4 sm:space-x-9">
-					<img alt="Aleph.im logo" src="../../assets/logos/aleph.svg" />
-					<img alt="TwentySixCloud logo" src="../../assets/logos/twentysix.svg" />
+					<a href="https://aleph.im" target="_blank">
+						<img alt="Aleph.im logo" src="../../assets/logos/aleph.svg" />
+					</a>
+					<a href="https://twentysix.cloud" target="_blank">
+						<img alt="TwentySixCloud logo" src="../../assets/logos/twentysix.svg" />
+					</a>
 				</div>
 			</div>
 		</div>
 	</section>
 </template>
-<script setup lang="ts"></script>
+<script lang="ts" setup></script>
