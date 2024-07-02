0d7ffc745d4557aa5d4466feb54c682d96547d4a
Tom BP <bariteaupeter.tom@gmail.com>
2024-07-02 13:16:09 +0200

    Hotfix - clarity and homogeneity (#71)
    * fix: add titles and uniform spaces between sections
    
    * fix: some spacing + text colors
    
    * fix: padding on OpenSourceModelsSection

--- src/pages/Home/AICustomizationSection.vue
+++ src/pages/Home/AICustomizationSection.vue
@@ -6,6 +6,7 @@ import VirtualBoyfriend from "../../assets/home/personas/VirtualBoyfriend.png";
 import VirtualGirlfriend from "../../assets/home/personas/VirtualGirlfriend.png";
 import Tromp from "../../assets/home/personas/Tromp.png";
 import { onMounted, ref, watch } from "vue";
+import LTinyHeading from "../../components/LTinyHeading.vue";
 
 const AIs = [
 	{ text: "Donald Tromp", image: Tromp },
@@ -49,72 +50,76 @@ onMounted(() => {
 </script>
 
 <template>
-	<section class="flex justify-center overflow-hidden py-24">
-		<div class="relative flex h-full w-[1440px]">
-			<div class="absolute left-0 z-10 h-full w-64 bg-gradient-to-r from-neutral-white" />
-			<div class="absolute right-full z-20 h-full w-full bg-neutral-white" />
-			<div class="absolute left-full z-20 h-full w-full bg-neutral-white" />
-			<div class="relative flex h-full w-3/5 flex-col max-lg:w-full">
-				<div class="w-full items-center space-y-6 bg-neutral-white py-8 text-center max-lg:px-96 lg:hidden">
-					<h3 class="px-12 max-sm:px-32">AI Customization, Privacy-First with LibertAI</h3>
+	<section class="flex flex-col justify-center overflow-hidden pb-36 pt-24">
+		<LTinyHeading class="mb-12 text-primary max-lg:hidden">Privacy First</LTinyHeading>
+		<div class="flex items-center justify-center">
+			<div class="relative flex h-full w-[1440px]">
+				<div class="absolute left-0 z-10 h-full w-64 bg-gradient-to-r from-neutral-white" />
+				<div class="absolute right-full z-20 h-full w-full bg-neutral-white" />
+				<div class="absolute left-full z-20 h-full w-full bg-neutral-white" />
+				<div class="relative flex h-full w-3/5 flex-col max-lg:w-full">
+					<div class="w-full items-center space-y-6 bg-neutral-white py-20 text-center max-lg:px-96 lg:hidden">
+						<LTinyHeading>Privacy First</LTinyHeading>
+						<h3 class="px-12 text-primary max-sm:px-32">AI Customization, Privacy-First with LibertAI</h3>
 
-					<p class="body-small px-12 max-sm:px-32">
-						Customize AI assistants for smooth integration into your workflows without compromising your privacy,
-						keeping your data encrypted and under your control.
-					</p>
-					<div>
-						<a class="text-majorelle-500" href="https://chat.libertai.io/">
-							<LButton small text="Customize Now" />
-						</a>
+						<p class="body-small px-12 max-sm:px-32">
+							Customize AI assistants for smooth integration into your workflows without compromising your privacy,
+							keeping your data encrypted and under your control.
+						</p>
+						<div>
+							<a class="text-majorelle-500" href="https://chat.libertai.io/">
+								<LButton small text="Customize Now" />
+							</a>
+						</div>
 					</div>
-				</div>
-				<div
-					class="AIcard-container flex gap-6 transition-transform duration-500 ease-in-out"
-					@transitionend="isTransitioning = false"
-				>
 					<div
-						v-for="(ai, index) in [
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-							...AIs,
-						]"
-						:key="index"
-						class="AIcard flex w-72 flex-col items-center justify-center"
+						class="AIcard-container flex gap-6 transition-transform duration-500 ease-in-out"
+						@transitionend="isTransitioning = false"
 					>
-						<div class="flex h-140 w-72 flex-col items-center overflow-hidden rounded-2xl border bg-neutral-white">
-							<div class="relative top-0 h-124 w-full">
-								<img :alt="ai.text" :src="ai.image" class="absolute left-0 top-0 size-full object-cover" />
+						<div
+							v-for="(ai, index) in [
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+								...AIs,
+							]"
+							:key="index"
+							class="AIcard flex w-72 flex-col items-center justify-center"
+						>
+							<div class="flex h-140 w-72 flex-col items-center overflow-hidden rounded-2xl border bg-neutral-white">
+								<div class="relative top-0 h-124 w-full">
+									<img :alt="ai.text" :src="ai.image" class="absolute left-0 top-0 size-full object-cover" />
+								</div>
+								<p class="rich-bold-18 mt-4 text-majorelle-800">{{ ai.text }}</p>
 							</div>
-							<p class="rich-bold-18 mt-4 text-majorelle-800">{{ ai.text }}</p>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="z-20 h-full w-2/5 bg-neutral-white max-lg:hidden">
-				<div class="py w-128 space-y-8 px-6 py-24 max-xl:w-96">
-					<h2 class="max-xl:hidden">AI Customization, Privacy-First with LibertAI</h2>
-					<h3 class="xl:hidden">AI Customization, Privacy-First with LibertAI</h3>
-					<p class="body-small">
-						Customize AI assistants for smooth integration into your workflows without compromising your privacy,
-						keeping your data encrypted and under your control.
-					</p>
-					<div>
-						<a class="text-majorelle-500" href="https://chat.libertai.io/#/persona-management">
-							<LButton small text="Customize Now" />
-						</a>
+				<div class="z-20 h-full w-2/5 bg-neutral-white max-lg:hidden">
+					<div class="py w-128 space-y-8 px-6 py-24 max-xl:w-96">
+						<h2 class="text-primary max-xl:hidden">AI Customization, Privacy-First with LibertAI</h2>
+						<h3 class="xl:hidden">AI Customization, Privacy-First with LibertAI</h3>
+						<p class="body-small">
+							Customize AI assistants for smooth integration into your workflows without compromising your privacy,
+							keeping your data encrypted and under your control.
+						</p>
+						<div>
+							<a class="text-majorelle-500" href="https://chat.libertai.io/#/persona-management">
+								<LButton small text="Customize Now" />
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
