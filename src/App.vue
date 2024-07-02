673bfdbc706d6905a5aa4ae82a666db8ab027677
Reza Rahemtola <reza.rahemtola@poc-innovation.fr>
2024-07-02 15:45:44 +0200

    feat: Remove token unlock countdown (#57)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import { useGeneralStore } from "./stores/general";
-import Countdown from "./Countdown.vue";
-
-const store = useGeneralStore();
-const displaySite = () => {
-	store.showCountdown = false;
-	window.scrollTo(0, 0);
-};
 </script>
 
 <template>
-	<Navbar v-show="!store.isCountdownDisplayed" />
-	<Countdown v-if="store.isCountdownDisplayed" :action="displaySite" />
-
-	<RouterView v-else />
-
+	<Navbar />
+	<RouterView />
 	<Footer />
 </template>
