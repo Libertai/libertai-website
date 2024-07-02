ded8fcba06d2b213635ff88ffb5de126feddccb7
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-07-02 08:40:10 +0200

    feat: FeedBack Section

--- src/pages/Home/FeedbacksSection.vue
+++ src/pages/Home/FeedbacksSection.vue
@@ -1,44 +1,60 @@
 <script lang="ts" setup>
-import jonathanSchemoul from "../../assets/icons/JonathanSchemoul.png";
-import shaneP from "../../assets/icons/ShaneP.png";
+import ShanePhoto from "../../assets/icons/ShaneP.png";
+import JonathanPhoto from "../../assets/icons/JonathanSchemoul.png";
+import LButton from "../../components/LButton.vue";
 
 const feedbacks = [
-	// {
-	// 	name: "Nicolas Bacca",
-	// 	title: "Ledger Co-Founder",
-	// 	quote:
-	// 		'"The world of technology thrives best when individuals are left alone to be different, creative, and disobedient."',
-	// 	image: nicolasBacca,
-	// },
 	{
-		name: "Shane P",
+		id: 1,
+		name: "Shane P.",
 		title: "LibertAI Co-Founder",
-		quote:
+		feedback:
 			"“The future of AI should be open, transparent, and community-driven. Decentralization is the only way that makes that possible.”",
-		image: shaneP,
+		photo: ShanePhoto,
 	},
 	{
+		id: 2,
 		name: "Jonathan Schemoul",
 		title: "aleph.im CEO and Co-Founder",
-		quote:
+		feedback:
 			"“We built aleph.im to empower a new generation of apps. LibertAI's decentralized AI vision was a perfect match. Together, we can create a robust ecosystem that puts AI power in the hands of the people.”",
-		image: jonathanSchemoul,
+		photo: JonathanPhoto,
 	},
 ];
 </script>
 
 <template>
-	<section class="flex items-center justify-center p-40 max-lg:px-0 max-lg:py-16">
-		<div class="flex gap-8 max-lg:grid max-lg:grid-cols-2 max-sm:grid-cols-1">
-			<div v-for="feedback in feedbacks" :key="feedback.name" class="flex flex-col">
-				<div
-					class="flex h-[477px] w-[300px] flex-col justify-end rounded-2xl border bg-majorelle-500 p-4 text-neutral-white"
-				>
-					<p class="body-default my-auto text-center font-bold">{{ feedback.quote }}</p>
-					<img :src="feedback.image" alt="User" class="m-auto mb-2 mt-6 h-16 w-16 rounded-full" />
-					<p class="body-bold-default text-center">{{ feedback.name }}</p>
-					<p class="body-tiny text-center">{{ feedback.title }}</p>
-				</div>
+	<section class="relative flex justify-center">
+		<video
+			autoplay
+			muted
+			loop
+			class="absolute inset-0 h-full w-full object-cover"
+			src="../../assets/videos/Feedbacks.mp4"
+		/>
+		<div class="z-20 flex w-[1440px] items-center justify-center gap-6 py-60 max-lg:flex-col max-lg:px-0 max-lg:py-24">
+			<div
+				v-for="feedback in feedbacks"
+				class="flex h-100 w-72 flex-col justify-center rounded-3xl border-t bg-neutral-white bg-opacity-20 px-6 py-10 text-center max-lg:hidden"
+				:style="{
+					backdropFilter: 'blur(20px)',
+					transform: 'translateY(' + (feedback.id % 2 === 0 ? 1 : -1) * 10 + '%)',
+				}"
+			>
+				<p class="body-small text-majorelle-800">{{ feedback.feedback }}</p>
+				<img :src="feedback.photo" alt="Shane P." class="mx-auto mb-4 mt-8 h-16 w-16 rounded-full" />
+				<p class="rich-bold-18">{{ feedback.name }}</p>
+				<p class="body-tiny">{{ feedback.title }}</p>
+			</div>
+			<div
+				v-for="feedback in feedbacks"
+				class="flex h-100 w-72 flex-col justify-center rounded-3xl border-t bg-neutral-white bg-opacity-20 px-6 py-10 text-center lg:hidden"
+				:style="{ backdropFilter: 'blur(20px)' }"
+			>
+				<p class="body-small">{{ feedback.feedback }}</p>
+				<img :src="feedback.photo" alt="Shane P." class="mx-auto mb-4 mt-8 h-16 w-16 rounded-full" />
+				<p class="rich-bold-18">{{ feedback.name }}</p>
+				<p class="body-tiny">{{ feedback.title }}</p>
 			</div>
 		</div>
 	</section>
