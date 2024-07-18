05d7619cc1864faf8948da6ef9113c4c92d524ab
Croos3r <dorian.moy@epitech.eu>
2024-07-18 11:19:35 +0200

    feat(redirection-warning): added LLink component to proxy every link by a warning if it goes outside of the current site

---
+++ src/components/LLink.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { LinkHTMLAttributes, ref } from "vue";
+import { AlertDialog, AlertDialogContent, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { AlertDialogOverlay, AlertDialogPortal } from "radix-vue";
+import LButton from "@/components/LButton.vue";
+
+interface IProps extends /* @vue-ignore */ LinkHTMLAttributes {}
+
+withDefaults(defineProps<IProps>(), {});
+
+const open = ref(false);
+</script>
+
+<template>
+	<RouterLink v-if="href && href.startsWith('/')" :to="href">
+		<slot />
+	</RouterLink>
+	<AlertDialog v-model:open="open">
+		<AlertDialogTrigger as-child>
+			<a>
+				<slot />
+			</a>
+		</AlertDialogTrigger>
+		<AlertDialogPortal>
+			<AlertDialogOverlay class="fixed inset-0 z-30 backdrop-blur-sm" />
+			<AlertDialogContent class="flex flex-col gap-y-6 bg-primary px-6 py-9 text-neutral-white">
+				<h6 class="body-default text-center font-bold">Leaving our website</h6>
+				<p class="body-tiny text-center">
+					You will be redirected to an external website <strong class="font-bold">({{ $attrs.href }})</strong> that is
+					not under our control. We cannot guarantee the accuracy, security, or reliability of the information provided
+					on these external sites.
+				</p>
+				<div class="gap-x- flex justify-center gap-x-3">
+					<a v-bind="$attrs">
+						<LButton class="w-[150px]" light text="Continue" @click="open = false" />
+					</a>
+					<LButton class="w-[150px]" light text="Cancel" @click="open = false" />
+				</div>
+			</AlertDialogContent>
+		</AlertDialogPortal>
+	</AlertDialog>
+</template>
+
+<style scoped></style>
