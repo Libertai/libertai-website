7a809b580f365f3239a083ccc703fb3c782e0965
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-05-23 13:33:20 +0900

    feat: add smooth scroll

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,18 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { Navbar } from "@/components/Navbar.tsx";
+import ReactLenis from "lenis/react";
 
 export const Route = createRootRoute({
 	component: () => (
-		<>
+		<ReactLenis
+			root
+			options={{
+				lerp: 0.1,
+				duration: 1.4,
+			}}
+		>
 			<Navbar />
 			<Outlet />
-		</>
+		</ReactLenis>
 	),
 });
