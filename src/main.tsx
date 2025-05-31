41bf958d9cc726416362935735849ecfe538a944
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-05-31 18:50:21 +0900

    feat: Add scroll restoration

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+	routeTree,
+	scrollRestoration: true,
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
