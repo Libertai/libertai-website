5e4be8ea7a0c7bb180101a5d02a5cb39fd3404a6
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-05-31 21:00:36 +0900

    feat(tokenomics): Burn section

--- src/routes/tokenomics.tsx
+++ src/routes/tokenomics.tsx
@@ -3,6 +3,7 @@ import { TokenomicsHero } from "@/components/tokenomics/TokenomicsHero";
 import { TokenomicsDetails } from "@/components/tokenomics/TokenomicsDetails";
 import { TokenDistribution } from "@/components/tokenomics/TokenDistribution";
 import { ContractAddresses } from "@/components/tokenomics/ContractAddresses";
+import { BurnSection } from "@/components/tokenomics/BurnSection";
 import { FooterSection } from "@/components/FooterSection.tsx";
 
 export const Route = createFileRoute("/tokenomics")({
@@ -16,6 +17,7 @@ function TokenomicsPage() {
 			<TokenomicsDetails />
 			<ContractAddresses />
 			<TokenDistribution />
+			<BurnSection />
 			<FooterSection />
 		</div>
 	);
